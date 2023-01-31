b059084c545ec1c42089d4a60414bbf89621f4ba
junseinagao <junsei.nagao@gmail.com>
2023-01-31 13:28:31 +0900

    test: 🧪 add 'Draw!' is shown when game is draw

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Game", () => {
-  test("'Winner: ${winner name}' is shown when game finished", async ({
+  test("'Winner: ${winner name}' is shown when game is finished", async ({
     page,
   }) => {
     await page.goto("http://localhost:4173/");
@@ -12,5 +12,19 @@ test.describe("Game", () => {
     await page.locator('[data-test="square-button-4"]').click();
     await page.locator('[data-test="square-button-2"]').click();
     await expect(page.locator(".status")).toHaveText(/Winner: ./);
+  });
+
+  test("'Draw!' is shown when game is draw. ", async ({ page }) => {
+    await page.goto("http://localhost:4173/");
+    await page.locator('[data-test="square-button-0"]').click();
+    await page.locator('[data-test="square-button-1"]').click();
+    await page.locator('[data-test="square-button-2"]').click();
+    await page.locator('[data-test="square-button-3"]').click();
+    await page.locator('[data-test="square-button-4"]').click();
+    await page.locator('[data-test="square-button-8"]').click();
+    await page.locator('[data-test="square-button-7"]').click();
+    await page.locator('[data-test="square-button-6"]').click();
+    await page.locator('[data-test="square-button-5"]').click();
+    await expect(page.locator(".status")).toHaveText("Draw!");
   });
 });
